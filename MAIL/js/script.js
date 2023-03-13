89f4d5c892264d4b90b0dd34308ac7341b48d27a
yaredghebre <yared2391@gmail.com>
2023-03-13 12:41:30 -0300

    Add: MAIL complete

--- MAIL/js/script.js
+++ MAIL/js/script.js
@@ -9,36 +9,48 @@
 // ALTRIMENTI, STAMPO "accesso negato, inserisci nuovamente.."
 
 // Assegno elementi da mostrare nel DOM:
-const userEmailInput = document.getElementById("user-email");
-
-const errorElem = document.getElementById("error-msg");
-const successElem = document.getElementById("success-msg");
-
 const submitBtn = document.getElementById("send-btn");
+submitBtn.addEventListener("click", function (){
+    const userEmailInput = document.getElementById("user-email");
 
-// Creo un array con 4 email:
-const emails = ["mariorossi@abc.com", "lucabianchi@xyz.it", "giuseppeverdi@123.org", "fabioneri@qwerty.net"]
-console.log(emails);
-
-// Chiedo di inserire la mail:
-const userEmail = userEmailInput.value;
-console.log(userEmail);
-
-let accepted = false;
-
-for (let i = 0; i <emails.length; i++) {
-    const currentEmail = emails[i];
+    const userEmailElem = document.getElementById("success-msg");
     
-    if (currentEmail === userEmail) {
-        accepted = true;
+    const errorElem = document.getElementById("error-msg");
+    const successElem = document.getElementById("success-msg");
+    
+    // Creo un array con 4 email:
+    const emails = ["mariorossi@abc.com", "lucabianchi@xyz.it", "giuseppeverdi@123.org", "fabioneri@qwerty.net"]
+    console.log(emails);
+    
+    // Chiedo di inserire la mail:
+    const userEmail = userEmailInput.value;
+    console.log(userEmail);
+    
+    let accepted = false;
+    
+    for (let i = 0; i < emails.length; i++) {
+        const currentEmail = emails[i];
+        
+        if (currentEmail === userEmail) {
+            accepted = true;
+        }
     }
-}
+    
+    if (accepted === true) {
+        console.log("Accesso consentito"); 
+        document.getElementById("success-msg").className="success";
+    
+    } else {
+        console.log("Accesso negato");
+        document.getElementById("error-msg").className="fail";
+    
+    }
 
-if (accepted === true) {
-    console.log("Accesso consentito");   
-} else {
-    console.log("Accesso negato");
-}
+});
+
+
+
+
 
 
 
